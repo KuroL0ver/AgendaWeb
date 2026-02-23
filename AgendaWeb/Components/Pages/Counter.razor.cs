37d7857d1d3ab548213bc8901137d373c6791519
Camila Fimbres <camifimbres@gmail.com>
2026-02-23 07:57:40 -0700

    Agregar archivos de proyecto.

---
+++ AgendaWeb/Components/Pages/Counter.razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace AgendaWeb.Components.Pages
+{
+    public partial class Counter
+    {
+        private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+
+            string connectionString = "Server=localhost;Database=AgendaDb;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            string query = "INSERT into Contactos (Nombre, Telefono, Email) values ('Desde VS', '6623789065', 'esme2@gmail.com')"; 
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.CommandType = CommandType.Text;
+
+
+            connection.Open();
+            command.ExecuteNonQuery();
+
+            connection.Close();
+            connection.Dispose();
+        }
+    }
+}
---
+++ AgendaWeb/Data/Commands/ContactoCommand.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace AgendaWeb.Data.Commands
+{
+    public class ContactoCommand
+    {
+        private readonly SQLServer _sqlServer;
+
+        public ContactoCommand(SQLServer sqlServer)
+        {
+            _sqlServer = sqlServer;
+        }
+
+        public int InsertarContacto(string nombre, string telefono, string email)
+        {
+            string query = "INSERT INTO Contactos " +
+                "(Nombre, Telefono, Email) " +
+                "VALUES " +
+                "(@Nombre, @Telefono, @Email)";
+            SqlParameter[] parameters = new SqlParameter[]
+            {
+                new SqlParameter("@Nombre", nombre),
+                new SqlParameter("@Telefono", telefono),
+                new SqlParameter("@Email", email)
+            };
+            return _sqlServer.NonQuery(query, parameters);
+        }
+
+        public int EliminarContacto(int id)
+        {
+            string query = "DELETE FROM Contactos WHERE Id = @Id";
+            SqlParameter[] parameters = new SqlParameter[]
+            {
+                new SqlParameter("@Id", id)
+            };
+            return _sqlServer.NonQuery(query, parameters);
+        }
+
+        public int ActualizarContacto(int id, string nombre, string telefono, string email)
+        {
+            string query = "UPDATE Contactos " +
+                "SET " +
+                "Nombre = @Nombre, " +
+                "Telefono = @Telefono, " +
+                "Email = @Email " +
+                "WHERE Id = @Id";
+            SqlParameter[] parameters = new SqlParameter[]
+            {
+                new SqlParameter("@Id", id),
+                new SqlParameter("@Nombre", nombre),
+                new SqlParameter("@Telefono", telefono),
+                new SqlParameter("@Email", email)
+            };
+            return _sqlServer.NonQuery(query, parameters);
+        }
+    }
+}
