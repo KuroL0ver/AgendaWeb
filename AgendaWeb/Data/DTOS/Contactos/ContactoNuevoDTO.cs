2f6349a2f6ebdc52df62a1432c5743fd874b23f9
Camila Fimbres <camifimbres@gmail.com>
2026-03-05 08:00:29 -0700

    Cambios de color, boton, y div

--- AgendaWeb/Data/DTOS/Contactos/ContactoNuevoDTO.cs
+++ AgendaWeb/Data/DTOS/Contactos/ContactoNuevoDTO.cs
@@ -1,9 +1,14 @@
-﻿namespace AgendaWeb.Data.DTOS.Contactos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AgendaWeb.Data.DTOS.Contactos
 {
     public class ContactoNuevoDTO
     {
+        [Required(ErrorMessage ="El campo nombre es requerido")]
         public string Nombre { get; set; }
         public string Telefono { get; set; }
+
+        [Required(ErrorMessage = "El campo Email es requerido")]
         public string Email { get; set; }
     }
 }
